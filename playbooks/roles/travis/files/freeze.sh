09ede1f105e724a20e0e2108af5802eb06434091
Matt Moriarity <matt@mattmoriarity.com>
2019-04-08 16:56:27 -0600

    Fix some instant clone issues
    Add clock updating to freeze script
    Use VMware tools command to update network info as soon as it's ready

--- playbooks/roles/travis/files/freeze.sh
+++ playbooks/roles/travis/files/freeze.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cat <<EOF | sudo tee /Library/Application\ Support/VMware\ Tools/tools.conf
-[guestinfo]
-poll-interval = 5
-EOF
-sudo killall vmware-tools-daemon
-
 echo "Disabling networking..."
 sudo kextunload /System/Library/Extensions/IONetworkingFamily.kext/Contents/PlugIns/Intel82574L.kext
 sleep 2
@@ -16,3 +10,14 @@ sudo /Library/Application\ Support/VMware\ Tools/vmware-tools-daemon --cmd="inst
 
 echo "Reenabling networking..."
 sudo kextload /System/Library/Extensions/IONetworkingFamily.kext/Contents/PlugIns/Intel82574L.kext
+
+echo "Waiting for IP..."
+while ! ifconfig en0 | grep 'inet 10'; do
+  sleep 1
+done
+
+echo "Updating guest info for networking..."
+sudo /Library/Application\ Support/VMware\ Tools/vmware-tools-cli info update network
+
+echo "Updating clock..."
+sudo sntp -sS pool.ntp.org
