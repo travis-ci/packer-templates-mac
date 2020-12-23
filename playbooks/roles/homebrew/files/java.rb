5145fb5f5ba1b61911a9533005c3e166319ebfdc
Cesar Souto <cesar@travis-ci.org>
2020-12-23 18:46:50 -0300

    Fix java install

--- playbooks/roles/homebrew/files/java.rb
+++ playbooks/roles/homebrew/files/java.rb
@@ -1,6 +1,6 @@
 cask "java" do
   version "15.0.1,9:51f4f36ad4ef43e39d0dfdbaf6549e32"
-  sha256 "aad54ae4766d82ebd0de5189e71bdc6cd7336f0d62fda5eba24f2072a6de34d0"
+  sha256 "e1d4868fb082d9202261c5a05251eded56fb805da2d641a65f604988b00b1979"
 
   url "https://download.java.net/java/GA/jdk#{version.before_comma}/#{version.after_colon}/#{version.after_comma.before_colon}/GPL/openjdk-#{version.before_comma}_osx-x64_bin.tar.gz"
   name "OpenJDK"
