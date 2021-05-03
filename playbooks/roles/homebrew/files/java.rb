0131400de646873c6a0dfdcd0ae62a5944d15b7c
Cesar Souto <cesar@travis-ci.org>
2021-05-03 09:33:26 -0300

    Adding openjdk 16

--- playbooks/roles/homebrew/files/java.rb
+++ playbooks/roles/homebrew/files/java.rb
@@ -1,6 +1,6 @@
 cask "java" do
-  version "15.0.1,9:51f4f36ad4ef43e39d0dfdbaf6549e32"
-  sha256 "e1d4868fb082d9202261c5a05251eded56fb805da2d641a65f604988b00b1979"
+  version "16.0.1,9:7147401fd7354114ac51ef3e1328291f"
+  sha256 "6098f839954439d4916444757c542c1b8778a32461706812d41cc8bbefce7f2f"
 
   url "https://download.java.net/java/GA/jdk#{version.before_comma}/#{version.after_colon}/#{version.after_comma.before_colon}/GPL/openjdk-#{version.before_comma}_osx-x64_bin.tar.gz"
   name "OpenJDK"
@@ -9,4 +9,3 @@ cask "java" do
 
   artifact "jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
 end
-
