bf7219be050dbdb8068921aa8c8e645d08f47a3e
Cesar Souto <cesar@travis-ci.org>
2021-09-23 00:09:17 -0300

    Xcode 13 files

--- playbooks/roles/homebrew/files/java.rb
+++ playbooks/roles/homebrew/files/java.rb
@@ -1,5 +1,5 @@
 cask "java" do
-  version "16.0.1,9:7147401fd7354114ac51ef3e1328291f"
+  version "17,35:0d483333a00540d886896bac774ff48b"
   sha256 "6098f839954439d4916444757c542c1b8778a32461706812d41cc8bbefce7f2f"
 
   url "https://download.java.net/java/GA/jdk#{version.before_comma}/#{version.after_colon}/#{version.after_comma.before_colon}/GPL/openjdk-#{version.before_comma}_osx-x64_bin.tar.gz"
@@ -9,3 +9,4 @@ cask "java" do
 
   artifact "jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
 end
+
